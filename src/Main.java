710771b65d48e156c75c6082e48709f7478a5189
SE/2017/003 <chathura_se17003@kln.ac.lk>
2019-10-31 14:22:35 +0530

    my first change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("my name is chinthaka chathuranga");
+        System.out.println("my name is chinthy");
     }
 }
