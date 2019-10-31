dd897423623408fae30896baf58e8cd7b2bbb357
SE/2017/003 <chathura_se17003@kln.ac.lk>
2019-10-31 14:58:43 +0530

    first day of oop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.net.SocketPermission;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("my name is chathuranga");
+
+        int op;
+        System.out.println("Welcome to University Management System");
+        System.out.println("");
+        System.out.println("Press [1] Register as a Student");
+        System.out.println("Press [2] Register as a Lecturer");
+        Scanner in=new Scanner(System.in);
+        Student s=new Student();
+        s.name=in.next();
+        s.age=in.nextInt();
+        op=in.nextInt();
     }
 }
