3c809e3de4c1fd83c01795dfa35dae2d2380c3e8
chinthakaseuok <kgcchathuranga@gmail.com>
2019-11-06 12:41:50 +0530

    new commit

--- src/Lecture.java
+++ src/Lecture.java
@@ -1,2 +1,41 @@
 public class Lecture {
+    private String name;
+    private int age;
+
+
+    public Lecture(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public Lecture() {
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,29 @@
-public class Student {
+public class Student
+{
+    private String stdName;
+    private int stdAge;
 
-    String name;
-    int age;
+
+    public Student() {
+        this.stdName = stdName;
+        this.stdAge = stdAge;
+    }
+
+    public String getStdName() {
+        return stdName;
+    }
+
+    public void setStdName(String stdName) {
+        this.stdName = stdName;
+    }
+
+    public int getStdAge() {
+        return stdAge;
+    }
+
+    public void setStdAge(int stdAge) {
+        this.stdAge = stdAge;
+    }
+
 
 }
